63977aa5ca2a60c9d98c34eba3bfbf1f3edf194c
Nkosi <internap627@gmail.com>
2020-04-11 17:47:43 -0400

    js cleanup

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   })
   return (
     <div className="App">
-      {stateString === 'logo' ? <div className='logo'>Gymie</div> : ''}
-      {stateString === 'trainers' ? <Trainers /> : ''}
+      {stateString === 'logo' ? <div className='logo'>Gymie</div> : <Trainers />}
+      {/* {stateString === 'trainers' ? <Trainers /> : ''} */}
     </div>
   );
 }
